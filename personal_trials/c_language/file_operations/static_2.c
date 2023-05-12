39cf4651d2c067adf290c1f34f3fb5ced45d3f9b
Wise-deldu <delduwise@gmail.com>
2023-05-12 22:58:19 +0000

    A program that employs the use of static variable

--- personal_trials/c_language/file_operations/static_2.c
+++ personal_trials/c_language/file_operations/static_2.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 
-int incrementAge() {
-    static int ages[3];
-    ages[0]++;
-    return ages[0];
+/**
+ * incrementAge - A program that employs the usage of static variables
+ * 
+ * Return: ages
+ */
+
+int incrementAge(void)
+{
+	static int ages[3];
+	ages[0]++;
+	return ages[0];
 }
 
-int main() {
-    int age1 = incrementAge();
-    printf("Age1: %d\n", age1);  // output: Age1: 1
-    
-    int age2 = incrementAge();
-    printf("Age2: %d\n", age2);  // output: Age2: 2
-    
-    int age3 = incrementAge();
-    printf("Age3: %d\n", age3);  // output: Age3: 3
+/**
+ * main - A program that calls the function of incrementAge.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int age1 = incrementAge();
+	printf("Age1: %d\n", age1);  /* output: Age1: 1 */
+
+	int age2 = incrementAge();
+
+	printf("Age2: %d\n", age2);  /* output: Age2: 2 */
+
+	int age3 = incrementAge();
+
+	printf("Age3: %d\n", age3);  /* output: Age3: 3 */
     
     return 0;
 }
